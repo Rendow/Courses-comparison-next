76c3f872745d279006cfbea6caf200e926d6f574
Vilde Ivan <rendow33@gmail.com>
2023-03-16 22:23:27 +0300

    footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,13 +1,22 @@
 import { FooterProps } from './Footer.props';
 import styles from './Footer.module.css';
 import cn from 'classnames';
+import {format} from 'date-fns';
 
 
-export const Footer = ({ children, ...props }: FooterProps): JSX.Element => {
+export const Footer = ({ children,className, ...props }: FooterProps): JSX.Element => {
 
 	return (
-		<div  {...props}>
-		Footer
-		</div>
+		<footer   className={cn(styles.footer, className)}>
+			<div>
+				OwlTop © 2019-{format(new Date(), 'yyyy')} Все права защищены
+			</div>
+			<a href='#'>
+				Пользовательское соглашение
+			</a>
+			<a  href='#'>
+				Политика конфиденциальности
+			</a>
+		</footer>
 	);
 };
